6be544743ad2dcdaa35cdab1126ca217b0f8b705
Marcin Kołodziej <marcinkolodziej15@gmail.com>
2018-10-21 23:04:28 +0200

    Added reducer info

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import thunk from 'redux-thunk';
 const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
 const rootReducer = combineReducers({
-
+    // YOUR REDUCERS HERE
 })
 
 const store = createStore(rootReducer, composeEnhancers(
