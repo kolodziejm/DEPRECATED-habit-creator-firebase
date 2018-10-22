8f01697664660144a82c29f27af03d13667d890b
Marcin Kołodziej <marcinkolodziej15@gmail.com>
2018-10-22 16:13:11 +0200

    Set up the firebase config file

--- src/fire.js
+++ src/fire.js
@@ -1,7 +1,12 @@
 import firebase from 'firebase/app';
 
 const config = {
-    // YOUR CONFIG HERE
+    apiKey: "AIzaSyBMQuJrCKlpGEDeZfDs_u9zB7tOXlmqMlU",
+    authDomain: "habit-creator.firebaseapp.com",
+    databaseURL: "https://habit-creator.firebaseio.com",
+    projectId: "habit-creator",
+    storageBucket: "habit-creator.appspot.com",
+    messagingSenderId: "63511062626"
 }
 
 const fire = firebase.initializeApp(config);
